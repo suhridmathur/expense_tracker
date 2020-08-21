eb099606cab4b441b49e95e9b1354353f81803b7
Suhrid Mathur <suhrid.mathur@industrybuying.com>
2020-08-21 14:07:01 +0530

    Auth Functionality Added

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -34,7 +34,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -113,3 +112,4 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_URL = "/login"
+LOGIN_REDIRECT_URL = "/"
--- users/views.py
+++ users/views.py
@@ -1,4 +1,31 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class Login(TemplateView):
     template_name = "login.html"
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email", None)
+        password = request.POST.get("password", None)
+        
+        if not (email and password):
+            message = "Please provide email and password"
+            return render(request, self.template_name, {"message": message})
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            message = f"User with email {email} does not exist"
+            return render(request, self.template_name, {"message": message})
+
+        user = authenticate(request, username=user.username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect("/")
+        else:
+            message = f"Bad Credentials!"
+            return render(request, self.template_name, {"message": message})
+        return HttpResponseRedirect("/login/")
